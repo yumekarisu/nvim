4d39cb18c773973d5d3d8c7a260bc502d73c0b3a
yumekarisu <alicevanaice@gmail.com>
2025-01-28 22:41:41 +0900

    Added mason for automatic LSP installations
    Added mason.nvim
    Added mason-lspconfig.nvim
    Added mason-tool-installer.nvim

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -2,6 +2,12 @@ return {
     {
 	"neovim/nvim-lspconfig",
 	dependencies = {
+	    {
+		"williamboman/mason.nvim",
+		opts = {},
+	    },
+	    "williamboman/mason-lspconfig.nvim",
+	    "WhoIsSethDaniel/mason-tool-installer.nvim",
 	    "saghen/blink.cmp",
 	    {
 		"folke/lazydev.nvim",
@@ -17,7 +23,20 @@ return {
 	},
 	config = function()
 	    local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+	    require('mason-tool-installer').setup {
+		ensure_installed = {
+		    "lua-language-server",
+		    "clangd"
+		}
+	    }
+	    require("mason-lspconfig").setup()
+
 	    require("lspconfig").lua_ls.setup {
+		capabilities = capabilities
+	    }
+
+	    require("lspconfig").clangd.setup {
 		capabilities = capabilities
 	    }
 	end
